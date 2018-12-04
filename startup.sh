6f4d8ce3efd139141a81791b4c6bcd570e4039d2
Ingo Boernig <iboernig@redhat.com>
2018-12-04 22:35:25 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 
 # download model if necessary
 
-if [ ! -d /deepspeach/models ]; then 
+if [ ! -d /deepspeech/models ]; then 
     wget https://github.com/mozilla/DeepSpeech/releases/download/v0.3.0/deepspeech-0.3.0-models.tar.gz -P /deepspeech
     tar xzf /deepspeech/deepspeech-0.3.0-models.tar.gz -C /deepspeech
     rm -f /deepspeech/deepspeech-0.3.0-models.tar
