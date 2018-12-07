23173cff1ecd75f8f905677705ccae49b906ec81
Cloud User <keith.tenzer@gmail.com>
2018-12-07 07:23:05 -0500

    trying express

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser({defer: true}));
 
   var form = new formidable.IncomingForm();
     //Formidable uploads to operating systems tmp dir by default
-    form.uploadDir = "./img";       //set upload directory
+    //form.uploadDir = "./img";       //set upload directory
     form.keepExtensions = true;     //keep file extension
 
     form.parse(req, function(err, fields, files) {
@@ -32,7 +32,7 @@ app.use(bodyParser({defer: true}));
 
         //Formidable changes the name of the uploaded file
         //Rename the file to its original name
-        fs.rename(files.fileUploaded.path, './img/'+files.fileUploaded.name, function(err) {
+        fs.rename(files.fileUploaded.path, '/app/'+files.fileUploaded.name, function(err) {
         if (err)
             throw err;
           console.log('renamed complete');  
