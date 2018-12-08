5812a6eb558f95a541456bdc40389a8151c20b8d
Fedora <fedora@ml-demo.novalocal>
2018-12-08 12:01:45 +0000

    fixed issue removing models tarballs

--- startup.sh
+++ startup.sh
@@ -4,19 +4,19 @@
 if [ ! -d /deepspeech/models ]; then 
     wget https://github.com/mozilla/DeepSpeech/releases/download/v0.3.0/deepspeech-0.3.0-models.tar.gz -P /deepspeech
     tar xzf /deepspeech/deepspeech-0.3.0-models.tar.gz -C /deepspeech
-    rm -f /deepspeech/deepspeech-0.3.0-models.tar
+    rm -f /deepspeech/deepspeech-0.3.0-models.tar.gz
 
     wget https://github.com/bakwc/JamSpell-models/raw/master/en.tar.gz -P /deepspeech/models
     tar xzf /deepspeech/models/en.tar.gz -C /deepspeech/models
-    rm -f /deepspeech/models/en.tar
+    rm -f /deepspeech/models/en.tar.gz
 
     wget https://github.com/bakwc/JamSpell-models/raw/master/fr.tar.gz -P /deepspeech/models
     tar xzf /deepspeech/models/fr.tar.gz -C /deepspeech/models
-    rm -f /deepspeech/models/fr.tar
+    rm -f /deepspeech/models/fr.tar.gz
 
     wget https://github.com/bakwc/JamSpell-models/raw/master/ru.tar.gz -P /deepspeech/models
     tar xzf /deepspeech/models/ru.tar.gz -C /deepspeech/models
-    rm -f /deepspeech/models/ru.tar
+    rm -f /deepspeech/models/ru.tar.gz
 fi
 
 npm start
