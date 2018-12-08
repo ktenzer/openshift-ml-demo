d5089ff52a407fd90b0a11159068eeba6bb5c9da
Fedora <fedora@ml-demo.novalocal>
2018-12-08 12:03:42 +0000

    fixed issue removing models tarballs

--- startup.sh
+++ startup.sh
@@ -6,17 +6,17 @@ if [ ! -d /deepspeech/models ]; then
     tar xzf /deepspeech/deepspeech-0.3.0-models.tar.gz -C /deepspeech
     rm -f /deepspeech/deepspeech-0.3.0-models.tar.gz
 
-    wget https://github.com/bakwc/JamSpell-models/raw/master/en.tar.gz -P /deepspeech/models
-    tar xzf /deepspeech/models/en.tar.gz -C /deepspeech/models
+    wget https://github.com/bakwc/JamSpell-models/raw/master/en.tar.gz -P /deepspeech
+    tar xzf /deepspeech/en.tar.gz -C /deepspeech/models
     rm -f /deepspeech/models/en.tar.gz
 
-    wget https://github.com/bakwc/JamSpell-models/raw/master/fr.tar.gz -P /deepspeech/models
-    tar xzf /deepspeech/models/fr.tar.gz -C /deepspeech/models
-    rm -f /deepspeech/models/fr.tar.gz
+    wget https://github.com/bakwc/JamSpell-models/raw/master/fr.tar.gz -P /deepspeech
+    tar xzf /deepspeech/fr.tar.gz -C /deepspeech/models
+    rm -f /deepspeech/fr.tar.gz
 
-    wget https://github.com/bakwc/JamSpell-models/raw/master/ru.tar.gz -P /deepspeech/models
-    tar xzf /deepspeech/models/ru.tar.gz -C /deepspeech/models
-    rm -f /deepspeech/models/ru.tar.gz
+    wget https://github.com/bakwc/JamSpell-models/raw/master/ru.tar.gz -P /deepspeech
+    tar xzf /deepspeech/ru.tar.gz -C /deepspeech/models
+    rm -f /deepspeech/ru.tar.gz
 fi
 
 npm start
