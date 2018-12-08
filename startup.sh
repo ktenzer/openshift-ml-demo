b5e1ec34c6629a42798fbd59de64cf3f6e79a053
Fedora <fedora@ml-demo.novalocal>
2018-12-08 13:02:17 +0000

    broke out model setup in startup script

--- startup.sh
+++ startup.sh
@@ -2,36 +2,37 @@
 
 # download models if necessary
 if [ ! -d /deepspeech/models ]; then 
-    sleep 3
+    echo "Downloading deepspeech models"
     wget https://github.com/mozilla/DeepSpeech/releases/download/v0.3.0/deepspeech-0.3.0-models.tar.gz -P /deepspeech
     tar xzf /deepspeech/deepspeech-0.3.0-models.tar.gz -C /deepspeech
     rm -f /deepspeech/deepspeech-0.3.0-models.tar.gz
 fi
 
 if [ ! -f /deepspeech/models/en.bin ]; then 
-    sleep 3
+    echo "Downloading en spelling models for jamspell"
     wget https://github.com/bakwc/JamSpell-models/raw/master/en.tar.gz -P /deepspeech
     tar xzf /deepspeech/en.tar.gz -C /deepspeech/models
     rm -f /deepspeech/en.tar.gz
 fi
 
 if [ ! -f /deepspeech/models/fr.bin ]; then 
-    sleep 3
+    echo "Downloading fr spelling models for jamspell"
     wget https://github.com/bakwc/JamSpell-models/raw/master/fr.tar.gz -P /deepspeech
     tar xzf /deepspeech/fr.tar.gz -C /deepspeech/models
     rm -f /deepspeech/fr.tar.gz
 fi
 
 if [ ! -f /deepspeech/models/ru.bin ]; then 
-    sleep 3
+    echo "Downloading ru spelling models for jamspell"
     wget https://github.com/bakwc/JamSpell-models/raw/master/ru.tar.gz -P /deepspeech
     tar xzf /deepspeech/ru.tar.gz -C /deepspeech/models
     rm -f /deepspeech/ru.tar.gz
 fi
 
 if [ -f /deepspeech/models/ru_small.bin ]; then 
-    sleep 3
+    echo "Renaming ru spell model"
    mv /deepspeech/models/ru_small.bin /deepspeech/models/ru.bin
 fi
 
+echo "Starting nodejs"
 npm start
