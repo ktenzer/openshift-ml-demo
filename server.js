9e3218766ea20430f2924ba5b7ec2b8246073cc5
Fedora <fedora@ml-demo.novalocal>
2018-12-10 12:58:33 +0000

    setting nodejs port to 8080

--- server.js
+++ server.js
@@ -105,6 +105,6 @@ app.route('/upload')
 });
 
 //start http listener
-var server = app.listen(80, function() {
+var server = app.listen(8080, function() {
 console.log('Listening on port %d', server.address().port);
 });
